bf1ab8e82fb79bd9adebeaa137b0b2e1711fbada
hoangminhtit <23.volehoangminh12a7.2023nht@gmail.com>
2026-01-02 22:42:06 +0700

    add MultiHeadAttention and PositionWiseFeedForward

---
+++ attention.py
@@ -0,0 +1,56 @@
+import torch
+import math
+import torch.nn as nn
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model: int, num_heads: int):
+        """
+        The initialization multi-head attention
+
+        Args:
+            d_model (int): dimensionality of the input.
+            num_heads (int): the number of attention heads to split the input into.
+        """
+        assert d_model % num_heads == 0, "d_model must be divisible by num heads"
+        
+        # Initialize dimensions
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_k = d_model // num_heads
+        
+        # Linear layers for transforming inputs
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_v = nn.Linear(d_model, d_model)
+        self.W_o = nn.Linear(d_model, d_model) # Output transformation
+    
+    def scaled_dot_product_attention(self, Q, K, V, mask=None):
+        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
+        
+        if mask is not None:
+            attn_scores = attn_scores.masked_fill(mask==0, -1e9)
+        
+        attn_probs = torch.softmax(attn_scores, dim=-1)
+        
+        output = torch.matmul(attn_probs, V)
+        return output
+    
+    def split_heads(self, x):
+        batch_size , seq_length, d_model = x.size()
+        out = x.view(batch_size, seq_length, self.num_heads, self.d_k)
+        return out
+    
+    def combine_heads(self, x):
+        batch_size, _, seq_length, d_k = x.size()
+        out = x.transpose(1, 2).contiguous().view(batch_size, seq_length, self.d_model)
+        return out
+    
+    def forward(self, Q, K, V, mask=None):
+        Q = self.split_heads(self.W_q(Q))
+        K = self.split_heads(self.W_k(K))
+        V = self.split_heads(self.W_v(V))
+        
+        # Perform scaled dot-product attention
+        attn_output = self.scaled_dot_product_attention(Q, K, V, mask=None)
+        output = self.W_o(self.combine_heads(attn_output))
+        return output
