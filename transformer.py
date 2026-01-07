a8916acd76a1cad65381dd27ab93eac88cf4837e
hoangminhtit <23.volehoangminh12a7.2023nht@gmail.com>
2026-01-07 11:01:08 +0700

    fix device issue

--- transformer.py
+++ transformer.py
@@ -32,7 +32,7 @@ class Transformer(nn.Module):
         src_mask = (src != 0).unsqueeze(1).unsqueeze(2)
         tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(2)
         seq_length = tgt.size(1)
-        nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1)).bool()
+        nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length, device=tgt.device), diagonal=1)).bool()
         tgt_mask = tgt_mask & nopeak_mask
         return src_mask, tgt_mask
     
